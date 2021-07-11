7511f3be78513c7a1c7fcace6d2d56251b3e0a9b
Bobby Steinbach <steinbach.rj@gmail.com>
2021-07-11 17:19:01 -0400

    upgrades gulp version and sets up a better development workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,45 @@
-var gulp = require('gulp'),
-fs = require('fs'),
-clean = require("gulp-clean"),
-uglify = require("gulp-uglify"),
-concat = require("gulp-concat"),
-header = require("gulp-header"),
-zip = require("gulp-zip"),
-runSequence = require('run-sequence');
- 
-var getVersion = function () {
-    info = require("./package.json");
-    return info.version;
-};
-var getCopyright = function () {
-    return fs.readFileSync('Copyright');
-};
+let { watch, series, src, dest } = require('gulp');
+var uglify = require("gulp-uglify"),
+    concat = require("gulp-concat"),
+    _clean = require("gulp-clean"),
+    batch = require('gulp-batch');
 
-gulp.task('js', function () {
-    // Concatenate and Minify JS
-    return gulp.src(['./src/blockrain.jquery.libs.js', './src/blockrain.jquery.src.js', './src/blockrain.jquery.themes.js'])
-    .pipe(concat('blockrain.jquery.js'))
-    .pipe(header(getCopyright(), {version: getVersion()}))
-    .pipe(gulp.dest('./dist'))
-    .pipe(uglify({preserveComments:'none'}))
-    .pipe(concat('blockrain.jquery.min.js'))
-    .pipe(header(getCopyright(), {version: getVersion()}))
-    .pipe(gulp.dest('./dist'));
-});
+function js() {
+    return src(['./src/blockrain.jquery.libs.js', './src/blockrain.jquery.src.js', './src/blockrain.jquery.themes.js'])
+        .pipe(concat('blockrain.jquery.js'))
+        .pipe(dest('./dist'))
+        .pipe(uglify({ preserveComments:'none' }))
+        .pipe(concat('blockrain.jquery.min.js'))
+        .pipe(dest('./dist'));
+}
 
-gulp.task('css', function () {
-    // CSS
-    return gulp.src(['./src/blockrain.css'])
-    .pipe(gulp.dest('./dist'));
-});
+function css() {
+    return src(['./src/blockrain.css'])
+        .pipe(dest('./dist'));
+}
 
-gulp.task('blocks', function () {
-    // CSS
-    return gulp.src(['./assets/blocks/custom/*.*'])
-    .pipe(gulp.dest('./dist/assets/blocks/custom'));
-});
+function blocks() {
+    return src(['./assets/blocks/custom/*.*'])
+        .pipe(dest('./dist/assets/blocks/custom'));
+}
 
-gulp.task('readme', function () {
-    // Readme
-    return gulp.src(['./README.md'])
-    .pipe(gulp.dest('./dist'));
-});
+function readme() {
+    return src(['./README.md'])
+        .pipe(dest('./dist'));
+}
 
-gulp.task('clean', function () {
-    return gulp.src('./dist', {read: false})
-    .pipe(clean());
-});
+function clean() {
+    return src('./dist', { read: false })
+       .pipe(_clean());
+}
 
-gulp.task('dist', function () {
-    // Create a ZIP File
-    return gulp.src(['./dist/**/*.*'])
-    .pipe(zip('blockrain.zip'))
-    .pipe(gulp.dest('./dist'));
-});
+function build() {
+    return series(clean, js, css, blocks, readme);
+}
 
+function dev() {
+    watch(['./src/*.js', './src/*.css'], series(js, css, blocks));
+}
 
-gulp.task('build', function(callback){
-  runSequence('clean', 
-              'js', 'css', 'blocks', 'readme', 'dist',
-              callback);
-});
-
-gulp.task('default', ['build']);
+exports.default = build;
+exports.watch = dev;
